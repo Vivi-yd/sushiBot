238606376e934bf4faef57f23acbb0282f5e02c8
Justin Dang <justin@justind.me>
2018-02-16 18:31:32 +1100

    Add draw card method

--- sushiGo.py
+++ sushiGo.py
@@ -2,12 +2,11 @@
 
 # 108 cards in a standard deck
 
+from numpy.random import choice
 
 class Game:
-    def __init__(self, player, rounds):
-        self.players = players
-        self.rounds = rounds
-    
+
+    #TODO refactor the instances variable.
     deck = {
         'tempura': 14,
         'dumpling': 14,
@@ -21,6 +20,90 @@ class Game:
         'wasabi': 6,
         'chopsticks': 4
     }
+    
+    hand1 = {
+        'tempura': 0,
+        'dumpling': 0,
+        'maki3': 0,
+        'maki2': 0,
+        'maki1': 0,
+        'sal_nigiri': 0,
+        'squ_nigiri': 0,
+        'egg_nigiri': 0,
+        'pudding': 0,
+        'wasabi': 0,
+        'chopsticks': 0
+    }
+
+    hand2 = {
+        'tempura': 0,
+        'dumpling': 0,
+        'maki3': 0,
+        'maki2': 0,
+        'maki1': 0,
+        'sal_nigiri': 0,
+        'squ_nigiri': 0,
+        'egg_nigiri': 0,
+        'pudding': 0,
+        'wasabi': 0,
+        'chopsticks': 0
+    }
+    
+    hand3 = {
+        'tempura': 0,
+        'dumpling': 0,
+        'maki3': 0,
+        'maki2': 0,
+        'maki1': 0,
+        'sal_nigiri': 0,
+        'squ_nigiri': 0,
+        'egg_nigiri': 0,
+        'pudding': 0,
+        'wasabi': 0,
+        'chopsticks': 0
+    }
+    
+    def __init__(self, player, rounds):
+        self.players = players
+        self.rounds = rounds
+        self.hand1 = self.draw_card(player)
+        self.hand2 = self.draw_card(player)
+        self.hand3 = self.draw_card(player)
+    
+    def draw_card(player):
+        
+        card_num = 12 - player  #Just a arbitary rule
+        card_list = []
+        deck_list = self.expand(self.deck)
+
+        hand = {
+            'tempura': 0,
+            'dumpling': 0,
+            'maki3': 0,
+            'maki2': 0,
+            'maki1': 0,
+            'sal_nigiri': 0,
+            'squ_nigiri': 0,
+            'egg_nigiri': 0,
+            'pudding': 0,
+            'wasabi': 0,
+            'chopsticks': 0
+        }
+        
+        card_list = choice(deck_list, card_num, replace=False)
+        
+        for card in card_list:
+            hand[card] += 1
+
+        return hand
+
+        
+    def expand_dict(self, deck):
+        expanded = []
+        for card, count in deck:
+            expanded.extend([card]*count)
+
+        return expanded
 
 
     # TODO
